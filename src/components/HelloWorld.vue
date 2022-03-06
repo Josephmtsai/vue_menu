bb7ff813e1a51f34f69f0ff21a34224c1f484664
joseph <mmx362003@gmail.com>
2022-03-06 11:05:14 +0800

    Add tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div class="font-bold text-7xl text-green-500 justify-center items-center">hello</div>
+    <div class="hidden md:block text-7xl">showing</div>
   </div>
 </template>
 

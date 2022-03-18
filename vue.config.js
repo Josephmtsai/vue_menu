63f63d84f8bc908a4818cabd284535c2aabf4e42
joseph <mmx362003@gmail.com>
2022-03-18 12:13:24 +0800

    fix bundle issue
    add

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,28 @@
 const { defineConfig } = require('@vue/cli-service');
+
 const isProduction = process.env.NODE_ENV === 'production';
 const isPartialView = process.env.PARTIAL_VIEW;
-console.log();
-const fileName = isProduction && !isPartialView ? 'js/[name].[hash:8].js' : 'js/[name].js';
-const chunkFilename = isProduction && !isPartialView ? 'js/[name].[hash:8].js' : 'js/[name].js';
+const fileName = isProduction && !isPartialView ? 'js/[name].[contenthash:8].js' : 'js/[name].js';
+const chunkFilename = isProduction && !isPartialView ? 'js/[name].[contenthash:8].js' : 'js/[name].js';
 const cdnPublicPath = isPartialView ? './dist/thirdparty' : './dist';
+const cssFilename = isProduction && !isPartialView ? 'css/[name].[contenthash:8].css' : 'css/[name].css';
 const config = defineConfig({
   transpileDependencies: true,
   outputDir: cdnPublicPath,
   filenameHashing: true,
-  configureWebpack: (config) => {
-    config.output = Object.assign(config.output, {
+  configureWebpack: (webpackSetting) => {
+    webpackSetting.output = Object.assign(webpackSetting.output, {
       filename: fileName,
       chunkFilename: chunkFilename,
     });
   },
+  css: {
+    extract: {
+      filename: cssFilename,
+      chunkFilename: cssFilename,
+    },
+  },
 });
-
 if (isPartialView && isProduction) {
   config['pages'] = {
     partialview: {
@@ -26,5 +32,4 @@ if (isPartialView && isProduction) {
     },
   };
 }
-
 module.exports = config;

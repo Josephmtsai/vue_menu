42b26da6e5fa7cbd3e14075183ebb8dde9a1bcc5
joseph <mmx362003@gmail.com>
2022-04-05 00:29:06 +0800

    add uv

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,8 @@
     <h1>{{ msg }}</h1>
     <div class="font-bold text-7xl text-green-500 justify-center items-center">hello</div>
     <div class="hidden md:block md:text-7xl md:text-red-500 lg:block lg:text-9xl lg:text-green-500">showing</div>
+    <n-button @click="changeUV()">changeUV</n-button>
+    <n-button @click="changeUVByVue()">changeUVByVue</n-button>
   </div>
 </template>
 
@@ -11,6 +13,22 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      userData: {},
+    };
+  },
+  created() {
+    this.userData = window.uv.data;
+  },
+  methods: {
+    changeUV() {
+      window.uv.data = 456;
+    },
+    changeUVByVue() {
+      this.userData = 12345678;
+    },
   },
 };
 </script>

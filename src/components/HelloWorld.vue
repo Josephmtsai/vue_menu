1295c2d80089f37b35e294adda11e0237244de81
joseph <mmx362003@gmail.com>
2022-04-05 13:01:02 +0800

    add custom event

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,7 @@
     <h1>{{ msg }}</h1>
     <div class="font-bold text-7xl text-green-500 justify-center items-center">hello</div>
     <div class="hidden md:block md:text-7xl md:text-red-500 lg:block lg:text-9xl lg:text-green-500">showing</div>
-    <n-button @click="changeUV()">changeUV</n-button>
-    <n-button @click="changeUVByVue()">changeUVByVue</n-button>
+    <n-button @click="createEvent()">create Event</n-button>
   </div>
 </template>
 
@@ -23,13 +22,11 @@ export default {
     this.userData = window.uv.data;
   },
   methods: {
-    changeUV() {
-      window.uv.data = 456;
-      console.log(window.uv);
-    },
-    changeUVByVue() {
-      this.userData = 12345678;
-      console.log(window.uv);
+    createEvent() {
+      this.userData += 1234;
+      const eventAwesome = new CustomEvent('onUserDataUpdate', { detail: { data: this.userData } });
+      document.dispatchEvent(eventAwesome);
+      console.log('asd');
     },
   },
 };

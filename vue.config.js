2751076eefec999b4dc0c617b8a6420b67073b5f
joseph <mmx362003@gmail.com>
2022-06-01 00:03:22 +0800

    fix config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 
-const isProduction = process.env.NODE_ENV === 'production';
-const isPartialView = process.env.PARTIAL_VIEW === 'true';
-
-const fileName = isProduction && !isPartialView ? 'js/[name].[contenthash:8].js' : 'js/[name].js';
-const chunkFilename = isProduction && !isPartialView ? 'js/[name].[contenthash:8].js' : 'js/[name].js';
-const cdnPublicPath = isPartialView ? './dist/thirdparty' : './dist';
-const cssFilename = isProduction && !isPartialView ? 'css/[name].[contenthash:8].css' : 'css/[name].css';
+const fileName = 'js/[name].[contenthash:8].js';
+const chunkFilename = 'js/[name].[contenthash:8].js';
+const cdnPublicPath = './dist';
+const cssFilename = 'css/[name].[contenthash:8].css';
 const config = defineConfig({
   transpileDependencies: true,
   outputDir: cdnPublicPath,
   filenameHashing: true,
+  productionSourceMap: true,
   configureWebpack: (webpackSetting) => {
     webpackSetting.output = Object.assign(webpackSetting.output, {
       filename: fileName,
@@ -25,14 +23,4 @@ const config = defineConfig({
   },
 });
 
-if (isPartialView && isProduction) {
-  console.log('PartialView');
-  config['pages'] = {
-    index: {
-      entry: './src/headerFooter.js',
-      template: './public/index.html',
-      filename: 'Index.htm',
-    },
-  };
-}
 module.exports = config;
